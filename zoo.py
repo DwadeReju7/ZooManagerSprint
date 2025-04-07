e9370476db7b553072a9101979bf05c6d6f61637
Dante' Wade <dantewade@Dantes-Laptop.attlocal.net>
2025-04-07 17:46:59 -0400

    zoo.py

--- zoo.py
+++ zoo.py
@@ -3,14 +3,18 @@
 class Zoo:
     def __init__(self):
         self.animals = []
+
     def add_animal(self, animal):
-        Print(f"Added {animal.name} to the zoo")
+        self.animals.append(animal)
+        print(f"Added {animal.name} to the zoo")
+
     def remove_animal(self, animal_name_or_id):
         for animal in self.animals:
             if animal.name == animal_name_or_id:
+                self.animals.remove(animal)
                 print(f"Remove {animal.name}")
                 return
-                print(f"Animal '{animal_name_or_id}' not found.")
+        print(f"Animal '{animal_name_or_id}' not found.")
 
     def list_animals(self):
         if not self.animals:
